d99dde6338d2b1e1a3fe28cb57d352075036128d
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-06-14 23:15:22 -0400

    just getting started

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-// @ts-check
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
 import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexjustesen.com/',
-
-  integrations: [
-    preact(),
-    sitemap(),
-    tailwind(),
-    robotsTxt()
-  ]
+  site: 'https://alexjustesen.com',
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()]
 });
