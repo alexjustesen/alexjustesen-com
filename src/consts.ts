7bf90c5e908c8749dc72b280624646441cf3bc36
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-06-28 11:46:42 -0400

    linktree instead of blog

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Hello, I'm Alex, | AlexJustesen.com";
-export const SITE_DESCRIPTION = "I'm a Business Intelligence Web Engineer at MassMutual by day and a keeper of too many projects by night.";
